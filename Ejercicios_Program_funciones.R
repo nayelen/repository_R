88c9f773b250cd8fe63b824ff35b6f18d88dc7d2
nayelen <ayelen.310@hotmail.com>
2025-03-26 16:51:08 +0100

    add new file program_funciones

---
+++ Ejercicios_Program_funciones.R
@@ -0,0 +1,59 @@
+# Ejercicio 1: Enunciado: Define una función llamada saludo que imprima en la consola el
+# mensaje "Hola, bienvenido a R".
+
+saludo <- function(){
+  print("Hola, bienvenido a R ")
+}
+saludo()
+
+# Ejercicio 2: Enunciado: Crea una función llamada calificar edad que tome un parámetro
+# numérico llamado edad y muestre en la consola si la persona es "menor de edad" o "mayor de edad".
+
+calificar_edad <- function(edad){
+  if(edad < 18){
+    print("Menor de edad")
+  }else {
+    print("Mayor de edad")
+  }
+}
+calificar_edad(16)
+
+# Ejercicio 3: Enunciado: Define una función llamada tabla multiplicar que tome un
+# parámetro numérico n e imprima la tabla de multiplicar del 1 al 10 de ese número.
+
+tabla_multiplicar <- function(n){
+  for (i in 1:10) {
+    resultado <- n * i 
+    print(paste(n,"x",i, "=", resultado))
+  }
+}
+tabla_multiplicar(8)
+
+# Ejercicio 4: Enunciado: Crea una función llamada numeros_pares que tome un parámetro 
+# numérico limite e imprima los números pares hasta ese límite.
+
+numeros_pares <- function(limite){
+  pares <- c()
+  for (i in 1:limite) {
+    if(i %% 2 == 0){
+      pares <- c(pares, i)
+    }
+  }
+  print(pares)
+}
+numeros_pares(20)
+
+# Ejercicio 5:Enunciado: Define una función llamada matriz cuadrada que tome un parámetro
+# numérico n e imprima una matriz cuadrada de tamaño n x n donde los
+# elementos son el resultado de la suma de sus índices de fila y columna.
+
+matriz_cuadrada <- function(n){
+  matriz <- matrix(0, nrow = n, ncol = n)
+  for(i in 1:n){
+    for (j in 1:n) {
+      matriz[i,j] <- i + j
+    }
+  }
+  print(matriz)
+}
+matriz_cuadrada(3)
